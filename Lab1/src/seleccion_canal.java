637ec06f800af933830623af2929b39c35519634
eimer <eimer@DESKTOP-P6RAMN3>
2020-09-28 12:29:25 -0500

    commit2: se quito boton show

--- Lab1/src/seleccion_canal.java
+++ Lab1/src/seleccion_canal.java
@@ -23,7 +23,36 @@ public class seleccion_canal extends JPanel
 		
 		    JPanel buttonPanel1 = new JPanel(new FlowLayout());
 		    buttonPanel1.setLayout(new BoxLayout(buttonPanel1, BoxLayout.Y_AXIS));
+		    
+		    JPanel buttonPanel2 = new JPanel(new FlowLayout());
+		    buttonPanel2.setLayout(new BoxLayout(buttonPanel2, BoxLayout.Y_AXIS));
 		
+	    
+		    final DefaultComboBoxModel panelName = new DefaultComboBoxModel();
+		
+		    panelName.addElement("Análogo");
+		    panelName.addElement("Digital");
+		    
+		    final JComboBox listCombo = new JComboBox(panelName);    
+		    listCombo.setSelectedIndex(1);
+		
+		    JScrollPane listComboScrollPane = new JScrollPane(listCombo);    
+		
+		    //JButton showButton = new JButton("Show");
+		
+		    listCombo.addActionListener(new ActionListener() {
+		       public void actionPerformed(ActionEvent e) { 
+		          String data = "";
+		          if (listCombo.getSelectedIndex() != -1) {  
+		             CardLayout cardLayout = (CardLayout)(panel.getLayout());
+		             cardLayout.show(panel,(String)listCombo.getItemAt(listCombo.getSelectedIndex()));	               
+		          }              
+		          statusLabel.setText(data);
+		       }
+		    }); 
+		    
+		    ///////////////////////////
+		    //buttonPanel1.add(listCombo);
 		    buttonPanel1.add(new JButton("Canal 1"));
 		    buttonPanel1.add(new JButton("Canal 2"));
 		    buttonPanel1.add(new JButton("Canal 3"));
@@ -34,47 +63,19 @@ public class seleccion_canal extends JPanel
 		    buttonPanel1.add(new JButton("Canal 8"));
 		    
 		    
-		    JPanel buttonPanel2 = new JPanel(new FlowLayout());
-		    buttonPanel2.setLayout(new BoxLayout(buttonPanel2, BoxLayout.Y_AXIS));
-		    
+		    //buttonPanel2.add(listCombo);
 		    buttonPanel2.add(new JButton("Canal 1"));
 		    buttonPanel2.add(new JButton("Canal 2"));
 		    buttonPanel2.add(new JButton("Canal 3"));
 		    buttonPanel2.add(new JButton("Canal 4"));
-			/*
-			 * JPanel textBoxPanel = new JPanel(new FlowLayout());
-			 * 
-			 * textBoxPanel.add(new JLabel("Name:")); textBoxPanel.add(new JTextField(20));
-			 */
-		
+		    
+		    
+		    //panel.add(listCombo);
 		    panel.add("Análogo", buttonPanel1);
 		    panel.add("Digital", buttonPanel2);
-		    
-		    final DefaultComboBoxModel panelName = new DefaultComboBoxModel();
-		
-		    panelName.addElement("Análogo");
-		    panelName.addElement("Digital");
-		    
-		    final JComboBox listCombo = new JComboBox(panelName);    
-		    listCombo.setSelectedIndex(0);
-		
-		    JScrollPane listComboScrollPane = new JScrollPane(listCombo);    
-		
-		    JButton showButton = new JButton("Show");
-		
-		    showButton.addActionListener(new ActionListener() {
-		       public void actionPerformed(ActionEvent e) { 
-		          String data = "";
-		          if (listCombo.getSelectedIndex() != -1) {  
-		             CardLayout cardLayout = (CardLayout)(panel.getLayout());
-		             cardLayout.show(panel,(String)listCombo.getItemAt(listCombo.getSelectedIndex()));	               
-		          }              
-		          statusLabel.setText(data);
-		       }
-		    }); 
 			  
 		    this.add(listComboScrollPane);
-		    this.add(showButton);
+		   // this.add(showButton);
 			this.add(panel);
 		}
 
